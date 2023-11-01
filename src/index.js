63c52a067503d8f0ee553882afacfa687af7f390
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2023-11-01 14:24:52 +0200

    analytics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { inject } from "@vercel/analytics";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+inject();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
